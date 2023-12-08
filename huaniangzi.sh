f93a883004e8c1cd781068c245ecfaf674c50184
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2023-12-08 17:21:01 +0800

    Update huaniangzi.sh

--- huaniangzi.sh
+++ huaniangzi.sh
@@ -2246,6 +2246,7 @@ case $choice in
         echo "13. Cloudreve网盘系统                   14. 简单图床图片管理程序"
         echo "15. emby多媒体管理系统                  16. Speedtest测速服务面板"
         echo "17. AdGuardHome去广告软件               18. onlyoffice在线办公OFFICE"
+        echo "19. portainer容器管理面板               20. VScode网页版"
         echo "------------------------"
         echo "0. 返回主菜单"
         echo "------------------------"
@@ -3878,6 +3879,187 @@ case $choice in
               fi
 
                 ;;
+              
+            19)
+              if docker inspect portainer &>/dev/null; then
+  
+                      clear
+                      echo "portainer已安装，访问地址: "
+                      external_ip=$(curl -s ipv4.ip.sb)
+                      echo "http:$external_ip:9050"
+                      echo ""
+  
+                      echo "应用操作"
+                      echo "------------------------"
+                      echo "1. 更新应用             2. 卸载应用"
+                      echo "------------------------"
+                      echo "0. 返回上一级选单"
+                      echo "------------------------"
+                      read -p "请输入你的选择: " sub_choice
+  
+                      case $sub_choice in
+                          1)
+                              clear
+                              docker rm -f portainer
+                              docker rmi -f portainer/portainer
+                              install_docker
+  
+                              docker run -d \
+                              --name portainer \
+                              -p 9050:9000 \
+                              -v /var/run/docker.sock:/var/run/docker.sock \
+                              -v /home/docker/portainer:/data \
+                              --restart always \
+                              portainer/portainer
+  
+  
+                              clear
+                              echo "portainer已经安装完成"
+                              echo "------------------------"
+                              echo "您可以使用以下地址访问:"
+                              external_ip=$(curl -s ipv4.ip.sb)
+                              echo "http:$external_ip:9050"
+                              echo ""
+                              ;;
+                          2)
+                              clear
+                              docker rm -f portainer
+                              docker rmi -f portainer/portainer
+                              rm -rf /home/docker/portainer
+                              echo "应用已卸载"
+                              ;;
+                          0)
+                              break  # 跳出循环，退出菜单
+                              ;;
+                          *)
+                              break  # 跳出循环，退出菜单
+                              ;;
+                      esac
+              else
+                  clear
+                  echo "安装提示"
+                  echo "portainer是一个轻量级的docker容器管理面板"
+                  echo "官网介绍: https://www.portainer.io/"
+                  echo ""
+  
+                  # 提示用户确认安装
+                  read -p "确定安装吗？(Y/N): " choice
+                  case "$choice" in
+                      [Yy])
+                      clear
+                      install_docker
+  
+                      docker run -d \
+                      --name portainer \
+                      -p 9050:9000 \
+                      -v /var/run/docker.sock:/var/run/docker.sock \
+                      -v /home/docker/portainer:/data \
+                      --restart always \
+                      portainer/portainer
+  
+                      clear
+                      echo "portainer已经安装完成"
+                      echo "------------------------"
+                      echo "您可以使用以下地址访问:"
+                      external_ip=$(curl -s ipv4.ip.sb)
+                      echo "http:$external_ip:9050"
+                      echo ""
+  
+                          ;;
+                      [Nn])
+                          ;;
+                      *)
+                          ;;
+                  esac
+              fi
+  
+                ;;
+
+            20)
+              if docker inspect vscode-web &>/dev/null; then
+  
+                      clear
+                      echo "VScode已安装，访问地址: "
+                      external_ip=$(curl -s ipv4.ip.sb)
+                      echo "http:$external_ip:8180"
+                      echo ""
+  
+                      echo "应用操作"
+                      echo "------------------------"
+                      echo "1. 更新应用             2. 卸载应用"
+                      echo "------------------------"
+                      echo "0. 返回上一级选单"
+                      echo "------------------------"
+                      read -p "请输入你的选择: " sub_choice
+  
+                      case $sub_choice in
+                          1)
+                              clear
+                              docker rm -f vscode-web
+                              docker rmi -f codercom/code-server
+                              install_docker
+  
+                              docker run -d -p 8180:8080 -v /home/docker/code-server:/home/coder/.local/share/code-server --name vscode-web --restart always codercom/code-server
+  
+                              clear
+                              echo "VScode已经安装完成"
+                              echo "------------------------"
+                              echo "您可以使用以下地址访问:"
+                              external_ip=$(curl -s ipv4.ip.sb)
+                              echo "http:$external_ip:8180"
+                              echo ""
+                              sleep 3
+                              docker exec vscode-web cat /home/coder/.config/code-server/config.yaml
+  
+                              ;;
+                          2)
+                              clear
+                              docker rm -f vscode-web
+                              docker rmi -f codercom/code-server
+                              rm -rf /home/docker/code-server
+                              echo "应用已卸载"
+                              ;;
+                          0)
+                              break  # 跳出循环，退出菜单
+                              ;;
+                          *)
+                              break  # 跳出循环，退出菜单
+                              ;;
+                      esac
+              else
+                  clear
+                  echo "安装提示"
+                  echo "VScode是一款强大的在线代码编写工具"
+                  echo "官网介绍: https://github.com/coder/code-server"
+                  echo ""
+  
+                  # 提示用户确认安装
+                  read -p "确定安装吗？(Y/N): " choice
+                  case "$choice" in
+                      [Yy])
+                      clear
+                      install_docker
+                      docker run -d -p 8180:8080 -v /home/docker/code-server:/home/coder/.local/share/code-server --name vscode-web --restart always codercom/code-server
+  
+                      clear
+                      echo "portainer已经安装完成"
+                      echo "------------------------"
+                      echo "您可以使用以下地址访问:"
+                      external_ip=$(curl -s ipv4.ip.sb)
+                      echo "http:$external_ip:8180"
+                      echo ""
+                      sleep 3
+                      docker exec vscode-web cat /home/coder/.config/code-server/config.yaml
+  
+                          ;;
+                      [Nn])
+                          ;;
+                      *)
+                          ;;
+                  esac
+              fi
+  
+                ;;
 
             0)
                 cd ~
@@ -5248,230 +5430,33 @@ EOF
     done
     ;;
   
-  99)
-    clear
+  00)
+    cd ~
+    curl -sS -O https://raw.githubusercontent.com/huaniangzi/sh/main/update_log.sh && chmod +x update_log.sh && ./update_log.sh
+    rm update_log.sh
+    echo ""
     curl -sS -O https://raw.githubusercontent.com/huaniangzi/sh/main/huaniangzi.sh && chmod +x huaniangzi.sh
     echo "脚本已更新到最新版本！"
     echo -e "\033[0;32m操作完成\033[0m"
     echo "按任意键继续..."
     read -n 1 -s -r -p ""
     echo ""
-    clear
     ./huaniangzi.sh
     exit
     ;;
 
-  00)
+  0)
     clear
-    echo "脚本更新日志"
-    echo  "------------------------"
-    echo "2023-10-01   v1.0.1"
-    echo "1.甲骨文云的DD脚本，添加了Ubuntu 20.04的重装选项。"
-    echo "2.LDNMP建站，开放了苹果CMS网站的搭建功能."
-    echo "3.系统信息查询，增加了内核版本显示，美化了界面。"
-    echo "4.甲骨文脚本中，添加了开启ROOT登录的选项。"
-    echo "------------------------"
-    echo "2023-10-01   v1.0.2"
-    echo "1.LDNMP建站，开放了独角数卡网站的搭建功能."
-    echo "2.LDNMP建站，优化了备份全站到远端服务器的稳定性."
-    echo "3.Docker管理，全局状态信息，添加了所有docker卷的显示."
-    echo "------------------------"
-    echo "2023-10-02   v1.0.3"
-    echo "Docker管理器全面升级，体验前所未有！"
-    echo "-加入了docker容器管理面板"
-    echo "-加入了docker镜像管理面板"
-    echo "-加入了docker网络管理面板"
-    echo "-加入了docker卷管理面板"
-    echo "-删除docker时追加确认信息，拒绝误操作"
-    echo "------------------------"
-    echo "2023-10-02   v1.0.4"
-    echo "1.新增了8选项，加入了常用面板工具合集！"
-    echo "-支持安装各种面板，包括: 宝塔，宝塔国际版，1panel，Nginx Proxy Manager等等，满足更多人群的使用需求！"
-    echo "2.优化了菜单效果"
-    echo "------------------------"
-    echo "2023-10-05   v1.0.3"
-    echo "新增了13选项，系统工具"
-    echo "花娘子一键脚本可以通过设置快捷键唤醒打开了，我设置的k作为脚本打开的快捷键！无需复制长命令了"
-    echo "加入了ROOT密码修改，切换成ROOT登录模式"
-    echo "系统设置中还有很多功能没开发，敬请期待！"
-    echo "------------------------"
-    echo "2023-10-06   v1.0.4"
-    echo "全面适配Centos系统，实现Ubuntu，Debian，Centos三大主流系统的适配"
-    echo "优化LDNMP中PHP输入数据最大时间，解决WordPress网站导入部分主题失败的问题"
-    echo "------------------------"
-    echo "2023-10-07   v1.0.5"
-    echo "docker管理中增加容器日志查看"
-    echo "选项13，系统工具中，加入了留言板的选项，可以留下你的宝贵意见也可以在这里聊天，贼好玩！"
-    echo "------------------------"
-    echo "2023-10-09   v1.0.6"
-    echo "优化了信息查询运行效率"
-    echo "信息查询新增了地理位置显示"
-    echo "优化了脚本内系统判断机制！"
-    echo "------------------------"
-    echo "2023-10-11   v1.0.7"
-    echo "LDNMP建站中加入了删除站点删除数据库功能"
-    echo "------------------------"
-    echo "2023-10-15   v1.0.8"
-    echo "选项8中，增加了一键搭建alist多存储文件列表工具的"
-    echo "选项8中，增加了一键搭建网页版乌班图远程桌面"
-    echo "选项10中，增加了开放所有端口功能"
-    echo "------------------------"
-    echo "2023-10-16   v1.0.9"
-    echo "系统信息查询中，终于可以显示总流量消耗了！总接收和总发送两个信息"
-    echo "------------------------"
-    echo "2023-10-22   v1.1.0"
-    echo "系统工具中新增SSH端口修改功能"
-    echo "系统工具中新增优化DNS地址功能"
-    echo "------------------------"
-    echo "2023-10-27   v1.1.1"
-    echo "系统性优化了代码，去除了无效的代码与空格"
-    echo "系统信息查询添加了系统时间"
-    echo "禁用ROOT账户，创建新的账户，更安全！"
-    echo "------------------------"
-    echo "2023-10-29   v1.1.2"
-    echo "LDNMP加入了安装bingchatAI聊天网站"
-    echo "面板工具中添加了哪吒探针脚本整合"
-    echo "------------------------"
-    echo "2023-11-02   v1.1.3"
-    echo "LDNMP加入了更新LDNMP选项"
-    echo "------------------------"
-    echo "2023-11-05   v1.1.4"
-    echo "面板工具添加安装QB离线BT磁力下载面板"
-    echo "优化IP获取源"
-    echo "------------------------"
-    echo "2023-11-08   v1.1.5"
-    echo "面板工具已安装的工具支持状态检测，可以进行删除了！"
-    echo "------------------------"
-    echo "2023-11-09   v1.1.6"
-    echo "系统工具中添加优先ipv4/ipv6选项"
-    echo "系统工具中添加查看端口占用状态选项"
-    echo "------------------------"
-    echo "2023-11-14   v1.1.7"
-    echo "LDNMP建站添加了定时自动远程备份功能"
-    echo "------------------------"
-    echo "2023-11-18   v1.1.8"
-    echo "面板工具增加了邮件服务器搭建，请确保服务器的25.80.443开放"
-    echo "------------------------"
-    echo "2023-11-22   v1.1.9"
-    echo "面板工具增加了聊天系统搭建"
-    echo "------------------------"
-    echo "2023-11-25   v1.2.0"
-    echo "面板工具增加了禅道项目管理软件搭建"
-    echo "------------------------"
-    echo "2023-11-28   v1.2.1"
-    echo "面板工具增加了青龙面板搭建"
-    echo "调整了面板工具列表的排版显示效果"
-    echo "------------------------"
-    echo "2023-11-29   v1.2.2"
-    echo "LDNMP大幅优化安装体验，添加安装进度条和百分比显示，太刁了！"
-    echo "------------------------"
-    echo "2023-12-01   v1.2.3"
-    echo "docker管理可以显示容器所属网络，并且可以加入网络和退出网络了"
-    echo "系统工具中增加修改虚拟内存大小的选项"
-    echo "系统信息查询中显示虚拟内存占用"
-    echo "------------------------"
-    echo "2023-12-02   v1.2.4"
-    echo "面板工具加入cloudreve网盘的搭建"
-    echo "面板工具加入简单图床程序搭建"
-    echo "LDNMP加入了高逼格的flarum论坛搭建"
-    echo "面板工具加入简单图床程序搭建"
-    echo "------------------------"
-    echo "2023-12-04   v1.2.5"
-    echo "LDNMP环境安装时用户密码将随机生成，提升安全性，安装环境更简单！"
-    echo "LDNMP环境安装时如果安装过docker将自动跳过，节省安装时间"
-    echo "LDNMP环境更新WordPress到6.3.1版本"
-    echo "添加了账户管理功能，查看当前账户列表，添加删除账户，账号权限管理等"
-    echo "------------------------"
-    echo "2023-12-6   v1.2.7"
-    echo "独角数卡登录时报错，显示解决办法"
-    echo "系统工具中添加随机用户密码生成器，方便懒得想用户名和密码的小伙伴"
-    echo "优化了所有搭建网站与面板后的信息复制体验"
-    echo "------------------------"
-    echo "2023-12-08   v1.2.8"
-    echo "面板工具中添加emby多媒体管理系统的搭建"
-    echo "LDNMP建站中可以搭建Bitwarden密码管理平台了"
-    echo "------------------------"
-    echo "2023-12-09   v1.2.9"
-    echo "LDNMP建站将站点信息查询和站点管理合并"
-    echo "LDNMP站点管理中添加证书重新申请和站点更换域名的功能"
-    echo "LDNMP建站增加了服务器与网站防护功能，防御暴力破解，防御网站被攻击"
-    echo "LDNMP建站优化了运行速度和安全性，增加了频率限制"
-    echo "LDNMP建站优化了防御程序的高可用性"
-    echo "------------------------"
-    echo "2023-12-11   v1.3.0"
-    echo "系统工具增加系统时区切换功能"
-    echo "LDNMP建站添加halo博客网站搭建"
-    echo "LDNMP建站添加优化LDNMP环境选项，可以开启高性能模式，大幅提升网站性能，应对高并发！"
-    echo "------------------------"
-    echo "2023-12-13   v1.3.1"
-    echo "面板工具增加了测速流量监控面板的安装"
-    echo "系统工具中添加开启BBR3加速功能"
-    echo "系统工具中优化BBR3加速安装流程，可根据CPU型号自行安装适合的内核版本"
-    echo "系统工具中BBRv3功能增加了更新内核和卸载内核功能"
-    echo "------------------------"
-    echo "2023-12-15   v1.3.2"
-    echo "开放端口相关优化"
-    echo "解决部分系统SSH端口切换后重启失联的问题"
-    echo "------------------------"
-    echo "2023-12-18   v1.3.3"
-    echo "LNMP建站管理中添加了站点缓存清理功能"
-    echo "面板工具中卸载对应应用时添加了应用目录一并删除，删除更彻底！"
-    echo "系统工具中修复了虚拟内存大小重启后还原的问题"
-    echo "------------------------"
-    echo "2023-12-19   v1.3.4"
-    echo "面板工具中增加AdGuardHome去广告软件安装和管理"
-    echo "系统工具添加了防火墙高级管理功能，可以开关端口，可以IP黑白名单"
-    echo "未来会上线地域黑白名单等高级功能"
-    echo "------------------------"
-    echo "2023-12-21   v1.3.5"
-    echo "系统工具中防火墙添加udp控制"
-    echo "测试脚本合集增加了缝合怪一条龙测试"
-    echo "系统信息查询中添加了系统运行时长显示"
-    echo "------------------------"
-    echo "2023-12-23   v1.3.6"
-    echo "LDNMP建站增加typecho轻量博客的搭建"
-    echo "面板工具中增加了在线office办公软件安装"
-    echo "LDNMP建站中增加仅安装nginx的选项专门服务于站点重定向和站点反向代理"
-    echo "精简无用的代码，优化执行效率"
-    echo "------------------------"
-    echo "2023-12-25   v1.3.7"
-    echo "LDNMP建站改用cerbot申请证书，更稳定更快速。弃用acme"
-    echo "面板工具修复QB无法登录问题"
-    echo "面板工具修复RocketChat进入后无限加载问题"
-    echo "系统工具中添加修改主机名功能"
-    echo "系统工具中添加服务器重启功能"
-    echo "------------------------"
-    echo "2023-12-27   v1.3.8"
-    echo "LDNMP建站过程中增加了nginx自我检测修复功能"
-    echo "系统工具添加更新源切换功能，请先在测试环境使用"
-    echo "LDNMP建站增加自定义上传静态html界面功能"
-    echo "------------------------"
-    echo "2023-12-28   v1.3.9"
-    echo "LDNMP建站中仅安装nginx功能添加安装成功提示，更优雅直观"
-    echo "LDNMP建站中仅安装nginx功能支持自动更新nginx版本"
-    echo "优化代码细节，定义调用函数，脚本执行更简洁，提升效率"
-    echo "------------------------"
-    echo "2023-12-29   v1.4.9"
-    echo "LDNMP在站点数据管理中增加查看站点分析报告功能，可以对网站流量进行监控与分析"
-    echo "主菜单添加手动更新脚本功能"
-    echo "------------------------"
-
+    exit
     ;;
-
-0)
-      clear
-      exit
-      ;;
 
   *)
     echo "无效的输入!"
-    ;;
-  esac
 
+esac
   echo -e "\033[0;32m操作完成\033[0m"
   echo "按任意键继续..."
   read -n 1 -s -r -p ""
   echo ""
   clear
 done
-
