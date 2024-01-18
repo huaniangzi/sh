742b6c36d1f2f5e0fb686cb8109a66e366b7f279
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-01-18 13:32:48 +0800

    Update huaniangzi.sh

--- huaniangzi.sh
+++ huaniangzi.sh
@@ -4296,6 +4296,8 @@ case $choice in
   10)
       clear
       wget -N --no-check-certificate https://raw.githubusercontent.com/MXCCO/lxdpro/main/lxdpro.sh && bash lxdpro.sh
+      huaniangzi
+      ;;
       ;;
 
   11)
