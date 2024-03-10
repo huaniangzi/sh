cc9b68d6c489baabd18cdd8d7ccc2ea53f18a2e0
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-03-10 15:51:17 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -2,9 +2,11 @@
 
 # Create a tar archive of the web directory
 cd /home/ && tar czvf web_$(date +"%Y%m%d%H%M%S").tar.gz web
+# Create a tar archive of the docker directory
+cd /home/ && tar czvf docker_$(date +"%Y%m%d%H%M%S").tar.gz docker
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /home/*.tar.gz | head -1 | xargs -I {} sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 {} root@0.0.0.0:/home/
+cd /home/ && ls -t /home/*.tar.gz | head -2 | xargs -I {} sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 {} root@0.0.0.0:/home/
 
 # Keep only 5 tar archives and delete the rest
 cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
