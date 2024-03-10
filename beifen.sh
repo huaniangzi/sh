fcce243a2c6b8011cb0bb62a95ab9c68192be18c
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-03-10 15:55:07 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -10,3 +10,9 @@ cd /home/ && ls -t /home/*.tar.gz | head -2 | xargs -I {} sshpass -p 123456 scp 
 
 # Keep only 5 tar archives and delete the rest
 cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+
+sshpass -p 123456 ssh -o StrictHostKeyChecking=no -p 22 root@0.0.0.0 << EOF
+cd /home/
+ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+EOF
+
