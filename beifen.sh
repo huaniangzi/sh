fcd2b56287bb28402aa7c6f07c69549fa0a5b64c
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-03-18 23:03:58 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -9,10 +9,10 @@ cd /home/ && tar czvf docker_$(date +"%Y%m%d%H%M%S").tar.gz docker
 cd /home/ && ls -t /home/*.tar.gz | head -2 | xargs -I {} sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 {} root@0.0.0.0:/home/
 
 # Keep only 5 tar archives and delete the rest
-cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+cd /home/ && ls -t /home/*.tar.gz | tail -n +3 | xargs -I {} rm {}
 
 sshpass -p 123456 ssh -o StrictHostKeyChecking=no -p 22 root@0.0.0.0 << EOF
 cd /home/
-ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+ls -t /home/*.tar.gz | tail -n +3 | xargs -I {} rm {}
 EOF
 
