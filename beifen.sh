0fb91a59e654d54c2605d9aebf5b2d3fc3f7dfca
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-03-22 17:03:41 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -1,18 +1,17 @@
 #!/bin/bash 
 
-# Create a tar archive of the web directory
-cd /home/ && tar czvf web_$(date +"%Y%m%d%H%M%S").tar.gz web
-# Create a tar archive of the docker directory
-cd /home/ && tar czvf docker_$(date +"%Y%m%d%H%M%S").tar.gz docker
+# Create a tar archive of the home directory
+cd / && tar czvf /home/home_$(date +"%Y%m%d%H%M%S").tar.gz home
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /home/*.tar.gz | head -2 | xargs -I {} sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 {} root@0.0.0.0:/home/
+latest_backup=$(ls -t /home_* | head -1)
+sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 "$latest_backup" root@0.0.0.0:/home/
 
-# Keep only 5 tar archives and delete the rest
-cd /home/ && ls -t /home/*.tar.gz | tail -n +3 | xargs -I {} rm {}
-
+# Keep only 3 latest backups on remote server
 sshpass -p 123456 ssh -o StrictHostKeyChecking=no -p 22 root@0.0.0.0 << EOF
 cd /home/
-ls -t /home/*.tar.gz | tail -n +3 | xargs -I {} rm {}
+ls -t /home_*.tar.gz | tail -n +4 | xargs -I {} rm {}
 EOF
 
+# Delete all other backups except the latest one
+rm /home_*.tar.gz
