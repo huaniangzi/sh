7b7a4fc5c32ac9c78568c78377823749ef432cdb
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-03-24 15:55:39 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -4,7 +4,14 @@
 cd /home/ && tar czvf home_$(date +"%Y%m%d%H%M%S").tar.gz *
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /*.tar.gz | head -1 | xargs -I {} sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 {} root@0.0.0.0:/home/
+cd /home/ && tar_file=$(ls -t *.tar.gz | head -1) && \
+sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 "$tar_file" root@0.0.0.0:/home/
 
-# Keep only 5 tar archives and delete the rest
-cd /home/ && ls -t /*.tar.gz | tail -n +4 | xargs -I {} rm {}
+# Keep only 4 tar archives on the remote server and delete the rest
+sshpass -p 123456 ssh -o StrictHostKeyChecking=no -p 22 root@0.0.0.0 << EOF
+cd /home/
+ls -t *.tar.gz | tail -n +5 | xargs -I {} rm {}
+EOF
+
+# Delete all locally created tar archives
+rm /home/*.tar.gz
