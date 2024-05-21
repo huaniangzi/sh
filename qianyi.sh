c6d96540cb23d2186cdc3b24f9a4004c8a84642a
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-05-22 00:45:21 +0800

    Update qianyi.sh

--- qianyi.sh
+++ qianyi.sh
@@ -4,7 +4,7 @@
 sshpass -p 123456 ssh -o StrictHostKeyChecking=no root@0.0.0.0 "mkdir -p /home/web"
 
 # 清空目标服务器上的文件夹内容
-sshpass -p 123456 ssh -o StrictHostKeyChecking=no root@0.0.0.0 "rm -rf /home/web/*"
+sshpass -p 123456 ssh -o StrictHostKeyChecking=no root@0.0.0.0 "rm -rf /home/web"
 
 # 将本地目录传输到目标服务器
-sshpass -p 123456 scp -r -o StrictHostKeyChecking=no /home/web/ root@0.0.0.0:/home/web
+sshpass -p 123456 scp -r -o StrictHostKeyChecking=no /home/web root@0.0.0.0:/home/web
