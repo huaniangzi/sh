cac0049630167d7b4f8e3866ece76a4879f01a68
花娘子 <116478602+huaniangzi@users.noreply.github.com>
2024-05-21 15:34:47 +0800

    Update qianyi.sh

--- qianyi.sh
+++ qianyi.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# 目标服务器相关信息
-target_server_ip="目标服务器IP"
-remote_folder="/home/web"
-username="root"
-password="远程服务器密码"
-
 # 清空目标服务器上的文件夹内容
-sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$target_server_ip" "rm -rf $remote_folder/*"
-
-# 本地目录
-local_folder="/home/web"
+sshpass -p 123456 ssh -o StrictHostKeyChecking=no root@0.0.0.0 rm -rf /home/web
 
 # 将本地目录传输到目标服务器
-sshpass -p "$password" scp -r -o StrictHostKeyChecking=no "$local_folder" "$username@$target_server_ip:$remote_folder"
+sshpass -p 123456 scp -r -o StrictHostKeyChecking=no root@0.0.0.0:/home/web
